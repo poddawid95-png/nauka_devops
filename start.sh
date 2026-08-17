1d39fcc483a7a1410b9610a02a73cc2d33cd3a81
Dawid <poddawid95@gmail.com>
2026-08-17 16:29:34 +0200

    Poprawiono literowke w start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ zawod="DevOps"
 echo "Cześć $imie"
 echo "Uczę się $zawod"
 echo "Uczę się Git i DevOps"
-echo "Testuj nowa galaz"D
+echo "Testuj nowa galaz"
 echo "Uczę sie GitHub i DevOps"
 echo "Testuje branch test"
